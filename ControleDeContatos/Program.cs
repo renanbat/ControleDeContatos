cd3b87394c98f20f737dd2a862cbeeb244f7f19e
renan <renan@DESKTOP-UAVPIT3>
2022-11-22 10:07:44 -0300

    Add project files.

---
+++ ControleDeContatos/Program.cs
@@ -0,0 +1,34 @@
+global using ControleDeContatos.Data;
+global using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BancoContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
